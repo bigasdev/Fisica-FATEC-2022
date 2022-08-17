3b672faa17bb25f6f8704f5a5e124d971d8b3905
GUSTAVO CASSIO DOMINGUES <0040962013027@americana.fatec.br>
2022-08-16 22:15:42 -0300

    first commit

---
+++ Assets/Scripts/PhysicsBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsBehaviour : MonoBehaviour
+{
+    [SerializeField] Vector3 accelerationRate;
+    [SerializeField] Vector3 currentAcceleration;
+
+    [SerializeField] float moveSpeed = 3f;
+
+    [SerializeField] AnimationCurve smoothRate;
+    [SerializeField] Transform wayPoint;
+
+    void FixedUpdate(){
+        AddAcceleration();
+
+        this.transform.position += currentAcceleration * Time.deltaTime;
+
+        /*var d = smoothRate.Evaluate(Time.time);
+        Debug.log(d);/
+        this.transform.position = Vector3.MoveTowards(this.transform.position, wayPoint.position, d * moveSpeed * Time.deltaTime);*/
+    }
+
+    void AddAcceleration(){
+        currentAcceleration += accelerationRate * Time.deltaTime;
+    }
+}
